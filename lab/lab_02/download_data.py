573b98ddf9978424528d8414a77b1a3444da0dce
Gerome Meyer <meyerger@students.zhaw.ch>
2022-10-21 16:42:01 +0200

    Removed download_data.py

--- lab/lab_02/download_data.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-response = requests.get(
-    'https://data.stadt-zuerich.ch/dataset/ted_taz_verkehrszaehlungen_werte_fussgaenger_velo/download/2022_verkehrszaehlungen_werte_fussgaenger_velo.csv')
-
-if not os.path.exists('data.csv'):
-    with open('data.csv', mode='wb') as f:
-        f.write(response.content)
